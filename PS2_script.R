9656ad3b11ed6d320c635ecbec41d9d39eda7b21
davidmtereriu <davidmterner@gmail.com>
2021-10-03 01:07:19 -0400

    Added figures; completed questions 1-5.

--- PS2_script.R
+++ PS2_script.R
@@ -73,10 +73,52 @@ num_deriv_fun=function(x,f_x){
   return(df_dx)
 }
 
+lv_solver_feild=function(A,B,C,D){
+  equilib<-data.frame(number=c(1,2),
+                      x=c(0,C/D),
+                      y=c(0,A/B)) 
+  x_eq=C/D
+  y_eq=A/B
+  
+  #lim1=max(x_eq,y_eq)
+  
+  x_seq=seq(0.1*x_eq,2*x_eq,length.out=25)
+  y_seq=seq(0.1*y_eq,2*y_eq,length.out=25)
+  
+  graph_df=expand.grid(x=x_seq,y=y_seq)%>%
+    as.data.frame()%>%mutate(dx=(A-B*y)*x,
+                             dy=(D*x-C)*y,
+                             xend=x+dx/sd(dx),
+                             yend=y+dy/sd(dy),
+                             K=y^A*exp(-B*y)*x^C*exp(-D*x))
+  mes1=paste0("Note: (A=",A,"; B=",B,"; C=",C,"; D=",D,")")
+  mes2=paste("(Equilibrium prey/predator(s): ",x_eq, " and ", y_eq,")")
+  
+  g1=graph_df%>%ggplot(aes(x,y,fill=K))+
+    geom_raster(interpolate = TRUE)+
+    geom_segment(aes(xend=xend,yend=yend),
+                 color="black",
+                 arrow = arrow(length = unit(0.01, "npc")))+
+    scale_fill_viridis_c()+
+    xlim(0,2*x_eq)+
+    ylim(0,2*y_eq)+
+    ggthemes::theme_tufte()+
+    labs(x="Prey",y="Predator",fill="Energy",
+         title="Lotka–Volterra Phase Diagram",
+         subtitle = mes2,
+         caption=mes1)+
+    geom_point(x=x_eq,y=y_eq,color="red")
+  
+  
+  return(g1)
+}
+
 
 ################################################################################
-# Question 1: 
+# Testing Functions
 ################################################################################
+if(FALSE){
+
 dt1=0.001
 time_max=54
 iter_count=time_max/dt1
@@ -101,7 +143,7 @@ b1$pop_df%>%
 cycle_df=b1$pop_df%>%
   mutate(start_x=b1$equilib[2,"x"],
          start_y=b1$equilib[2,"y"],
-         theta=atan((Prey-start_x)/(Predator-start_y)))
+         theta=atan((Prey-start_y)/(Predator-start_x)))
 
 
 cycle_df=cycle_df%>%mutate(slope=(Predator-start_y)/(Prey-start_x))
@@ -245,6 +287,276 @@ period_test%>%
   facet_wrap(~series,scales = "free")+
   labs(x="Time",y="Pop",color="Species",title="Periodic Populations")+
   scale_color_manual(values = c("Red","Blue"))+
-  ggthemes::theme_clean()
+  ggthemes::theme_clean()}
 
 
+################################################################################
+# PS Questions
+################################################################################
+dt1=0.001
+time_max=12
+iter_count=time_max/dt1
+
+A1=1
+B1=0.5
+C1=2
+D1=1
+
+q1_a=lv_solver(x0=3,y0=5,A=A1,B=B1,C=C1,D=D1,iterations = iter_count,dt=dt1,conserve = F)
+
+A2=1.5
+
+q2_a=lv_solver(x0=3,y0=5,A=A2,B=B1,C=C1,D=D1,iterations = iter_count,dt=dt1,conserve = F)
+
+
+
+# Populations x(12) and y(10)
+q1_a$pop_df%>%select(time,Prey,Predator)%>%
+  filter(time %in% c(10,12))%>%round(3)
+
+q2_a$pop_df%>%select(time,Prey,Predator)%>%
+  filter(time %in% c(10,12))%>%round(3)
+
+#Plots 
+
+q1_a$plot+scale_x_continuous(limits = c(0,10),breaks = seq(0,10,1))+
+  scale_y_continuous(limits = c(0,6),breaks = seq(0,6,1))+
+  labs(title = "Prey x(t) and Predator y(t) Populations",
+       subtitle = "Note: a=1")+
+  geom_hline(yintercept = 0,color="purple",linetype="dashed")
+
+q2_a$plot+scale_x_continuous(limits = c(0,10),breaks = seq(0,10,1))+
+  scale_y_continuous(limits = c(0,6),breaks = seq(0,6,1))+
+  labs(title = "Prey x(t) and Predator y(t) Populations",
+       subtitle = "Note: a=1.5")+
+  geom_hline(yintercept = 0,color="purple",linetype="dashed")
+
+
+#Period testing 
+
+dt1=0.001
+time_max=100
+iter_count=time_max/dt1
+
+tictoc::tic()
+q2=lv_solver(x0=3,y0=5,A=A1,B=B1,C=C1,D=D1,iterations = iter_count,dt=dt1,conserve = F)
+tictoc::toc()
+
+q2$pop_df%>%
+  ggplot(aes(x=time,y=cons_ene))+
+  geom_line(color="darkgreen",size=1.2)+
+  theme_bw()+
+  labs(x="Time",y="K(t)/K(0)",title="Dissipating K over Time")
+
+
+simple_time=lm(data=q2$pop_df,cons_ene~time)
+summary(simple_time)
+
+
+q2$pop_df%>%
+  ggplot(aes(x=Prey,y=Predator,color=ene))+
+  geom_path()+
+  geom_point(aes(x=2,y=2),color="red",size=3)+
+  viridis::scale_color_viridis() + theme_bw()+
+  labs(x="Prey",y="Predator",color="Law of Motion (K)",
+       caption="Note: Equilibrium prey/redator population is (2,2)",
+       title="Phase-Space Diagram: Dissipating K")
+
+q2$pop_df[1,]%>%round(3)
+
+
+tictoc::tic()
+q3=lv_solver(x0=3,y0=5,A=A2,B=B1,C=C1,D=D1,iterations = iter_count,dt=dt1,conserve = F)
+tictoc::toc()
+
+
+
+cycle_df_a1=q2$pop_df%>%
+  mutate(start_x=q2$equilib[2,"x"],
+         start_y=q2$equilib[2,"y"],
+         theta=atan((Prey-start_y)/(Predator-start_x)))
+
+
+cycle_df_a2=q3$pop_df%>%
+  mutate(start_x=q3$equilib[2,"x"],
+         start_y=q3$equilib[2,"y"],
+         theta=atan((Prey-start_y)/(Predator-start_x)))
+
+
+
+theta_fun1=approxfun(x=cycle_df_a1$time,y=cycle_df_a1$theta)
+theta_fun2=approxfun(x=cycle_df_a2$time,y=cycle_df_a2$theta)
+
+
+theta_roots1=rootSolve::uniroot.all(theta_fun1,interval = c(min(cycle_df_a1$time),max(cycle_df_a1$time)),n=1000)
+theta_roots2=rootSolve::uniroot.all(theta_fun2,interval = c(min(cycle_df_a2$time),max(cycle_df_a2$time)),n=1000)
+
+
+theta_df1=data.frame(id=seq_along(theta_roots1),time=theta_roots1,theta=0)
+theta_df1$id<-(theta_df1$id-1) %%4
+
+theta_df2=data.frame(id=seq_along(theta_roots2),time=theta_roots2,theta=0)
+theta_df2$id<-(theta_df2$id-1)%%4
+
+
+index_find=data.frame()
+for(i in 1:length(theta_df1$time)){
+  index_find=rbind(index_find,
+                   data.frame(id=which.min(abs(theta_df1$time[i]-cycle_df_a1$time))))
+}
+
+
+cycle_df_a1%>%
+  filter(time<5.2)%>%
+  ggplot()+
+  geom_path(aes(x=Prey,y=Predator))+
+  geom_segment(data=cycle_df_a1[index_find$id,]%>%filter(time<5.2),
+               aes(x=start_x,xend=Prey,y=start_y,yend=Predator,
+                   color=as.factor(time)),size=1.2)+
+  geom_point(data=cycle_df_a1[index_find$id,]%>%filter(time<5.2),
+             aes(x=Prey,y=Predator,color=as.factor(time)),
+             size=4)+
+  labs(color="Root",title="Phase Diagram")+
+  theme_minimal()+
+  geom_point(data=q2$equilib[2,],aes(x=x,y=y),size=4)
+
+pi_scales <- math_format(.x * pi, format = function(x) x / pi)
+
+cycle_df_a1%>%
+  filter(time<5.2)%>%
+  ggplot(aes(x=time,y=theta))+
+  geom_line()+
+  geom_hline(yintercept = 0,linetype="dashed")+
+  geom_point(data=filter(theta_df1,time<5.2),
+             aes(x=time,y=theta,color=as.factor(round(time,3))),
+             size=3)+
+  ggrepel::geom_label_repel(data=filter(theta_df1,time<5.2),
+                            aes(x=time,y=theta,label=paste0("Root:",id)))+
+  scale_y_continuous(labels = pi_scales,
+                     breaks = seq(-pi / 2,  pi / 2, pi/4))+
+  labs(x="Time",y=TeX("$\\Theta(t)$"),color="Root",title="Theta Time Series")+
+  theme_minimal()
+
+
+
+theta_dif1=theta_df1%>%
+  group_by(id)%>%
+  mutate(period=time-lag(time))%>%
+  filter(is.na(period)==F)%>%
+  ungroup()
+
+theta_dif2=theta_df2%>%
+  group_by(id)%>%
+  mutate(period=time-lag(time))%>%
+  filter(is.na(period)==F)%>%
+  ungroup()
+
+
+rbind(theta_dif1%>%mutate(model="a=1"),
+      theta_dif2%>%mutate(model="a=1.5"))%>%
+  ggplot(aes(x=time,y=period))+
+  geom_point()+
+  geom_smooth(method="lm")+
+  facet_wrap(~model,scales = "free")+
+  labs(x="Time",y="Estimated Period")+
+  theme_bw()
+
+period_mod1=lm(data=theta_dif1,period~time)
+period_mod2=lm(data=theta_dif2,period~time)
+
+texreg::texreg(list(period_mod1,period_mod2),digits = 4,
+               stars = c(0.01,0.05,0.1))
+
+#Double Time
+(coef(period_mod1)[1]/coef(period_mod1)[2]%>%as.numeric())%>%round(0)
+(coef(period_mod2)[1]/coef(period_mod2)[2]%>%as.numeric())%>%round(0)
+
+#Max pop during first period
+cycle_df_a1%>%filter(as.numeric(coef(period_mod1)[1])>time)%>%
+  summarize(max_prey=max(Prey),
+            max_pred=max(Predator),
+            min_prey=min(Prey),
+            min_pred=min(Predator))%>%
+  round(3)
+
+
+
+cycle_df_a2%>%filter(as.numeric(coef(period_mod2)[1])>time)%>%
+  summarize(max_prey=max(Prey),
+            max_pred=max(Predator),
+            min_prey=min(Prey),
+            min_pred=min(Predator))%>%
+  round(3)
+
+
+
+
+
+cycle_df_a1%>%select(time,Prey,Predator)%>%
+  mutate(Predator=cummax(Predator),
+         Prey=cummax(Prey))%>%
+  filter(time>coef(period_mod1)[1])%>%
+  gather(key=Species,value="pop",-1)%>%
+  ggplot(aes(x=time,y=pop,color=Species))+
+  geom_line(size=1.2)+
+  labs(x="Time",y="Max Pop")+
+  scale_color_manual(values = c("Red","Blue"))+
+  theme_minimal()+
+  theme(legend.position = "top")
+
+
+
+cycle_df_a1%>%select(time,Prey,Predator)%>%
+  mutate(Predator=cummin(Predator),
+         Prey=cummin(Prey))%>%
+  filter(time>coef(period_mod1)[1])%>%
+  gather(key=Species,value="pop",-1)%>%
+  ggplot(aes(x=time,y=pop,color=Species))+
+  geom_line(size=1.2)+
+  labs(x="Time",y="Min Pop")+
+  scale_color_manual(values = c("Red","Blue"))+
+  theme_minimal()+
+  theme(legend.position = "top")
+
+
+cycle_df_a2%>%select(time,Prey,Predator)%>%
+  mutate(Predator=cummax(Predator),
+         Prey=cummax(Prey))%>%
+  filter(time>coef(period_mod2)[1])%>%
+  gather(key=Species,value="pop",-1)%>%
+  ggplot(aes(x=time,y=pop,color=Species))+
+  geom_line(size=1.2)+
+  labs(x="Time",y="Max Pop")+
+  scale_color_manual(values = c("Red","Blue"))+
+  theme_minimal()+
+  theme(legend.position = "top")
+
+
+cycle_df_a2%>%select(time,Prey,Predator)%>%
+  mutate(Predator=cummin(Predator),
+         Prey=cummin(Prey))%>%
+  filter(time>coef(period_mod2)[1])%>%
+  gather(key=Species,value="pop",-1)%>%
+  ggplot(aes(x=time,y=pop,color=Species))+
+  geom_line(size=1.2)+
+  labs(x="Time",y="Min Pop")+
+  scale_color_manual(values = c("Red","Blue"))+
+  theme_minimal()+
+  theme(legend.position = "top")
+
+
+#Extinction Event
+
+a1_min=cycle_df_a1%>%select(time,Prey,Predator)%>%
+  mutate(Predator=cummin(Predator),
+         Prey=cummin(Prey))%>%
+  filter(time>coef(period_mod1)[1])
+ 
+a2_min=cycle_df_a2%>%select(time,Prey,Predator)%>%
+  mutate(Predator=cummin(Predator),
+         Prey=cummin(Prey))%>%
+  filter(time>coef(period_mod1)[1])
+
+
+extinct_mod1=lm(data=a1_min,Predator~time)
+extinct_mod2=lm(data=a2_min,Predator~time)
